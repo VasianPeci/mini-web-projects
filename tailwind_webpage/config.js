11de02020d3c2fe07534c96ba480495ea4bafa83
VasianPeci <vasianpeci26@gmail.com>
2025-07-25 12:51:50 +0200

    Tailwind CSS Practice

---
+++ tailwind_webpage/config.js
@@ -0,0 +1,19 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                pale_orange: '#ffd9a6',
+                light_orange: '#fbb03b',
+                orange: '#f7931e'
+            },
+            fontFamily: {
+                sans: ['Josefin Sands', 'sans-serif'],
+                title: ['Playfair Display SC', 'serif']
+            },
+            backgroundImage: {
+                mobile: "url('bg_mobile.png')",
+                desktop: "url('bg_desktop.png')"
+            }
+        }
+    }
+}
